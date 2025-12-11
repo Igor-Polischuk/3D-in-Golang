297f2b9d69072ac64bfc0c641a95aa20174068dc
IgorPolischuk <ihor.polishyk@gmail.com>
2025-12-12 00:39:24 +0100

    Add Circle shape implementation with point containment check and rasterization method

---
+++ internals/shapes/circle.go
@@ -0,0 +1,19 @@
+package shapes
+
+import (
+	"gortex/internals/drawable"
+	"gortex/internals/geom"
+)
+
+type Circle struct {
+	Pos geom.Vector2
+	R   float32
+}
+
+func (c *Circle) IsPointWithin(x, y float32) bool {
+	return x*x+y*y < c.R
+}
+
+func (c *Circle) Rasterize(r drawable.Rasterizer) {
+	r.RasterCircle(float32(c.Pos.X), float32(c.Pos.Y), c.R)
+}
