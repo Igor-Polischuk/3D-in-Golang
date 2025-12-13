f795dcc6c9436f670c32896db5de315a0e14f873
IgorPolischuk <ihor.polishyk@gmail.com>
2025-12-13 20:47:54 +0100

    Integrate GLFW screen rendering into main function, allowing simultaneous rendering of shapes on both OpenGL and terminal screens. Update shape positions and enhance line drawing capabilities for improved visual output.

--- cmd/gortex/main.go
+++ cmd/gortex/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gortex/internal/camera"
 	"gortex/internal/geom"
+
+	"gortex/internal/screen/glfwscreen"
 	"gortex/internal/screen/tscreen"
 	"gortex/internal/shapes"
 
@@ -11,36 +13,37 @@ import (
 )
 
 func main() {
+
 	w, h, err := term.GetSize(0)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	cam := camera.GetCamera(0, 0, 1)
-	s := tscreen.InitTerminalScreen(w, h, &cam, ' ')
+	gl := glfwscreen.InitGLFWScreen(1080, 720, &cam)
+	ts := tscreen.InitTerminalScreen(w, h, &cam, ' ')
 
-	c := shapes.Circle{Pos: geom.Vector2{X: -1, Y: -1}, R: 0.5}
-	r := shapes.Rectangle{Pos: geom.Vector2{X: 0, Y: 0}, Size: geom.Vector2{X: 0.5, Y: 0.5}, Angle: 0}
-
-	zoomIn := false
+	c := shapes.Circle{Pos: geom.Vector2{X: 1, Y: 1}, R: 0.5}
+	r := shapes.Rectangle{Pos: geom.Vector2{X: 0, Y: 0}, Size: geom.Vector2{X: 1, Y: 0.1}, Angle: 45}
+	r2 := shapes.Rectangle{Pos: geom.Vector2{X: -1, Y: 1}, Size: geom.Vector2{X: 1, Y: 0.1}, Angle: 0}
 
 	for {
-		s.BeginFrame()
-		s.RasterShape(r)
-		s.RasterShape(c)
-		s.Present()
+		gl.BeginFrame()
+		ts.BeginFrame()
 
-		if cam.Zoom > 7 {
-			zoomIn = true
-		} else if cam.Zoom < 0.05 {
-			zoomIn = false
-		}
+		gl.RasterShape(c)
+		ts.RasterShape(c)
+		gl.RasterShape(r)
+		gl.RasterShape(r2)
+		ts.RasterShape(r)
+		ts.RasterShape(r2)
 
-		if zoomIn {
-			cam.Zoom -= 0.05
-		} else {
-			cam.Zoom += 0.05
-		}
+		ts.DrawLine(0, 0, ts.Width, ts.Height, '~')
+		gl.DrawLine(0, 0, gl.W, gl.H, glfwscreen.GetColor(100, 100, 100))
+
+		gl.Present()
+		ts.Present()
 	}
 }
